001e5c00067fc3d7808c5de29c96d12eea94e0fd
Manju <74829190+mkumari10@users.noreply.github.com>
2021-01-03 15:50:03 +0530

    move the login js in login.js

--- ollywood game/ollygame.js
+++ ollywood game/ollygame.js
@@ -1,25 +1,5 @@
 $(document).ready(function(){
 
-
-//Login btn
-$('#loginbtn').on('click',function(e){
-   e.preventDefault();
-   $(e.target).hide();
-   let toShow = $(this).attr('href');
-   $(toShow).removeClass('d-none');
-});
-
-
-
-//login close btn
-$('.closebtn').on('click',function(e){
-         $('#Loginform').addClass('d-none');      
-         $('#loginbtn').show();
-         $('#rules').removeClass('col-sm-6 d-inline d-flex-column justify-content-center position-absolute rulepop');                  
-});
-
-
-
 //resizing the window
 $(window).resize(function(){
   var windowsize = $(window).width();
@@ -28,19 +8,8 @@ $(window).resize(function(){
 } 
 });
 
-
-
-//Start button in login form
-$('#start').on('click',function(e){
-   if($('input[name="pic"]:checked').attr('id') === 'catoname')
-   	$('#start').attr('href','r');
-   else
-   	$('#start').attr('href','Pictures.html');    
-});
-
-
-
-//rules and card popup div
+   
+   //rules and card popup div
 $('.score').on('click',function(e){
     let show = $(e.target).attr('href');
     if(show === "#rules")
